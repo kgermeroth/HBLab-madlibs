7d37ce4eb7d5146a624b4dd2f5ad9ab33dd8ce73
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-24 12:27:07 -0700

    completed madlib html template, finished create madlib function

--- madlibs.py
+++ madlibs.py
@@ -51,6 +51,17 @@ def show_madlib_form():
     else:
         return render_template("goodbye.html")
 
+@app.route('/madlib')
+def show_madlib():
+    """Taking user input and populating madlib"""
+
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", person=person, color=color, noun=noun,
+        adjective=adjective)
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
